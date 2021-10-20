ff1002fdd0407083e7bccd87d618ee9ee1b70eb5
shaffanFast <k190863@nu.edu.pk>
2021-10-20 12:48:37 +0500

    Updates

---
+++ precache-manifest.4b67ccf52b05ef29057c958e737839f7.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "0577be1508c35f847c5c",
+    "url": "/Addition_NumberLine/static/css/main.c706f64f.chunk.css"
+  },
+  {
+    "revision": "0577be1508c35f847c5c",
+    "url": "/Addition_NumberLine/static/js/main.4309e503.chunk.js"
+  },
+  {
+    "revision": "10cf8a96092dd6279477",
+    "url": "/Addition_NumberLine/static/js/runtime~main.2c6f339b.js"
+  },
+  {
+    "revision": "e0b9828889eb9c8701b2",
+    "url": "/Addition_NumberLine/static/css/2.a44f8451.chunk.css"
+  },
+  {
+    "revision": "e0b9828889eb9c8701b2",
+    "url": "/Addition_NumberLine/static/js/2.1e1bd938.chunk.js"
+  },
+  {
+    "revision": "10da4114f936b40ad994",
+    "url": "/Addition_NumberLine/static/js/3.e0fb1caa.chunk.js"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Addition_NumberLine/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Addition_NumberLine/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Addition_NumberLine/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Addition_NumberLine/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Addition_NumberLine/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Addition_NumberLine/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Addition_NumberLine/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Addition_NumberLine/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Addition_NumberLine/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Addition_NumberLine/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Addition_NumberLine/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/Addition_NumberLine/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "212c4bcb95a3ce810e745bff36eb4bf4",
+    "url": "/Addition_NumberLine/static/media/movingFishR.212c4bcb.gif"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/Addition_NumberLine/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "48d2eef01cecd55520ff1bf10c3e3748",
+    "url": "/Addition_NumberLine/static/media/movingFishL.48d2eef0.gif"
+  },
+  {
+    "revision": "a989d2c605289d1ad02c3ab3f3dd0981",
+    "url": "/Addition_NumberLine/static/media/more.a989d2c6.png"
+  },
+  {
+    "revision": "6f40b925808bcfceef28ee492480e926",
+    "url": "/Addition_NumberLine/static/media/less.6f40b925.png"
+  },
+  {
+    "revision": "5f1c11259cb15bc3800a9bb43a4a7242",
+    "url": "/Addition_NumberLine/static/media/background.5f1c1125.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/Addition_NumberLine/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/Addition_NumberLine/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/Addition_NumberLine/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/Addition_NumberLine/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/Addition_NumberLine/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Addition_NumberLine/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "c040d63c530ff46d273f95822ecb12e7",
+    "url": "/Addition_NumberLine/static/media/football10.c040d63c.png"
+  },
+  {
+    "revision": "c989b2046b4fa39573d918b056723605",
+    "url": "/Addition_NumberLine/static/media/football1.c989b204.png"
+  },
+  {
+    "revision": "fe892d45a0838c6408beabefe99b4aa6",
+    "url": "/Addition_NumberLine/static/media/bouncingFootball.fe892d45.wav"
+  },
+  {
+    "revision": "59843b2d65b09b144fd402b9327e37d1",
+    "url": "/Addition_NumberLine/static/media/instructions.59843b2d.gif"
+  },
+  {
+    "revision": "d9e398530b7031d5833f171d8888fc56",
+    "url": "/Addition_NumberLine/static/media/numberLine.d9e39853.png"
+  },
+  {
+    "revision": "ca4158825f66b0587acca1862b795eaf",
+    "url": "/Addition_NumberLine/static/media/numberLine2.ca415882.png"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Addition_NumberLine/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/Addition_NumberLine/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/Addition_NumberLine/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "47c968d9e5da0b78154ed3fcd39c0fe1",
+    "url": "/Addition_NumberLine/index.html"
+  }
+];
